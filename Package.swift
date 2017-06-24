7536f9e0e19906f9369b7e27354d1fe0291e18cf
Tomoya Kose <tomoya@mitsuse.jp>
2017-06-24 22:39:19 +0900

    Use the branch of APIKit for Linux.

--- Package.swift
+++ Package.swift
@@ -4,9 +4,13 @@ let package = Package(
     name: "Pocket",
     dependencies: [
         .Package(
-            url: "https://github.com/ishkawa/APIKit.git",
-            majorVersion: 3,
-            minor: 1
+            url: "https://github.com/mitsuse/APIKit.git",
+            Version(
+                3,
+                1,
+                2,
+                buildMetadataIdentifier: "linux"
+            )
         ),
         .Package(
             url: "https://github.com/ikesyo/Himotoki.git",
